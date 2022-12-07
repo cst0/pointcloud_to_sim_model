6c1cf6a4dee26d32041a74f4f4496be824d2fe76
Chris Thierauf <christopher.thierauf@tufts.edu>
2022-12-07 15:18:05 -0500

    change package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=["pointcloud2simmodel"],
+    packages=["real2sim"],
     package_dir={"": "src"},
 )
 
