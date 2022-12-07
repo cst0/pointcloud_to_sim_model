a18cad19baee85d78bb96d322f1a4024a1ff0f72
Chris Thierauf <christopher.thierauf@tufts.edu>
2022-12-07 15:15:09 -0500

    seperate client creation in pb

--- src/pointcloud2simmodel/scene_creation/spawner.py
+++ src/pointcloud2simmodel/scene_creation/spawner.py
@@ -2,12 +2,26 @@ import abc
 import os
 import rospy
 
+try:
+    import pybullet as p
+    import pybullet_data
+except ImportError:
+    rospy.logwarn("pybullet not installed, cannot spawn models with this strategy")
+
 from generator import Generator
 
+
 class Spawner(object):
+    def __init__(self, show_gui=False) -> None:
+        self.show_gui = show_gui
+        self.client = None
+        self.objects = []
+        self.start_locations = []
+
     @abc.abstractmethod
-    def spawn(self, generator:Generator):
+    def spawn(self, generator: Generator):
         raise NotImplementedError
+
 
 class GazeboSpawner(Spawner):
     def spawn(self, generator: Generator):
@@ -24,6 +38,7 @@ class GazeboSpawner(Spawner):
         finally:
             generator.close_file()
 
+
 class IgnitionSpawner(Spawner):
     def spawn(self, generator: Generator):
         openfile = generator.get_file()
@@ -39,28 +54,24 @@ class IgnitionSpawner(Spawner):
         finally:
             generator.close_file()
 
+
 class PybulletSpawner(Spawner):
+    def construct_client(self):
+        self.client = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0, 0, -9.8)
+        p.loadURDF("plane.urdf")
+        return self.client
+
     def spawn(self, generator: Generator):
-        client = None
+        if self.client is None:
+            self.construct_client()
+
         openfile = generator.get_file()
         try:
-            import pybullet as p
-            import pybullet_data
-
-            client = p.connect(p.GUI)
-            p.setAdditionalSearchPath(pybullet_data.getDataPath())
-            p.setGravity(0, 0, -9.8)
-            p.loadURDF("plane.urdf")
-            p.loadSDF(openfile.name)
-
-        except ImportError as e:
-            rospy.logerr(f"Couldn't import pybullet: {e}")
+            obj_id = p.loadSDF(openfile.name)
+            self.objects.append(obj_id)
         except Exception as e:
             rospy.logerr(f"Error spawning model: {e}")
         finally:
             generator.close_file()
-
-        if client is not None:
-            return client
-        else:
-            return None
